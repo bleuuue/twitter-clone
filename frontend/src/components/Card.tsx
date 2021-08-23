da78830e5f559cf781fe3bbf8e60b2c2dda78ddf
bleuuue <bleuuue@gmail.com>
2021-08-23 20:17:53 +0900

    dayjs

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,18 +1,28 @@
 import React, { FC } from 'react';
 import { ITweet } from '../interfaces';
 import ProfileIcon from './ProfileIcon';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
 
 interface CardProps {
   tweet: ITweet;
 }
 
 const Card: FC<CardProps> = ({ tweet }) => {
+  dayjs.extend(relativeTime);
+
   return (
     <div className="flex">
       <div>
         <ProfileIcon />
       </div>
-      <div className="font-noto">{tweet.users.nickname}</div>
+      <div>
+        <span className="font-bold">{tweet.users.nickname}</span>
+        <span className="ml-2 text-gray-500">
+          {dayjs(tweet.createdAt).locale('ko').fromNow()}
+        </span>
+      </div>
     </div>
   );
 };
