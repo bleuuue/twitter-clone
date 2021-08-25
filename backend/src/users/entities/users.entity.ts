792c5cfe6c8087ed2f663ba6b52df613e8e85cea
bleuuue <bleuuue@gmail.com>
2021-08-25 11:54:43 +0900

    like backend

--- backend/src/users/entities/users.entity.ts
+++ backend/src/users/entities/users.entity.ts
@@ -1,4 +1,6 @@
 import { Common } from 'src/common/common.entity';
+import { Likes } from 'src/likes/entities/likes.entity';
+import { LikesController } from 'src/likes/likes.controller';
 import { Tweets } from 'src/tweets/entities/tweets.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 
@@ -15,4 +17,7 @@ export class Users extends Common {
 
   @OneToMany(() => Tweets, (tweets) => tweets.users)
   tweets: Tweets[];
+
+  @OneToMany(() => Likes, (likes) => likes.user)
+  likes: Likes[];
 }
