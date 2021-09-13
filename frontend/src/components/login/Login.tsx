2d62e2ff7aa6ba3178188a0e3961484d8fb867a9
bleuuue <bleuuue@gmail.com>
2021-09-13 13:34:53 +0900

    sign up verify email

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -3,13 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import { FC, FormEvent, useEffect, useState } from 'react';
 import { useInput } from '../../hooks';
+import { toastError, toastSuccess } from '../../utils/toastify';
 
 const Login: FC = () => {
   const [passwordError, setPasswordError] = useState<string>('');
+  const [verifyToggle, setVerifyToggle] = useState<boolean>(false);
+
   const [signUpEmail, onChangeSignUpEmail] = useInput('');
   const [nickname, onChangeNickname] = useInput('');
   const [signUpPassword, onChangeSignUpPassword] = useInput('');
   const [passwordCheck, onChangePasswordCheck] = useInput('');
+  const [verifyCode, onChangeVerifyCode] = useInput('');
 
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
@@ -30,8 +34,8 @@ const Login: FC = () => {
       );
 
       if (response.statusText === 'Created') {
-        localStorage.setItem('token', response.data.token);
-        window.location.reload();
+        setVerifyToggle(true);
+        toastSuccess('인증코드를 발송했습니다.');
       }
     } catch (error) {
       console.error(error);
@@ -61,6 +65,28 @@ const Login: FC = () => {
     }
   };
 
+  const onSubmitVerifyCode = async (e: FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACK_URL}/auth/verify`,
+        {
+          email: signUpEmail,
+          verifyCode,
+        },
+      );
+
+      if (response.statusText === 'Created') {
+        localStorage.setItem('token', response.data.token);
+        window.location.reload();
+      }
+    } catch (error: any) {
+      console.error(error);
+      toastError(error.response.data.message);
+    }
+  };
+
   useEffect(() => {
     if (signUpPassword === passwordCheck) {
       setPasswordError('');
@@ -84,50 +110,72 @@ const Login: FC = () => {
         <div className="font-black text-6xl mb-4">Happening now</div>
         <div className="mb-8">
           <div className="font-bold text-4xl mb-2">Sign up</div>
-          <form onSubmit={onSubmitSignup}>
-            <input
-              className="input mb-2 w-96 text-2xl"
-              placeholder="Email"
-              type="text"
-              value={signUpEmail}
-              onChange={onChangeSignUpEmail}
-            />
-            <br />
-            <input
-              className="input mb-2 w-96 text-2xl"
-              placeholder="Nickname"
-              type="text"
-              value={nickname}
-              onChange={onChangeNickname}
-              maxLength={10}
-            />
-            <br />
-            <input
-              className="input mb-2 w-96 text-2xl"
-              placeholder="Password"
-              type="password"
-              value={signUpPassword}
-              onChange={onChangeSignUpPassword}
-            />
-            <br />
-            <input
-              className="input mb-2 w-96 text-2xl"
-              placeholder="Password check"
-              type="password"
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
-            />
-            <br />
-            <input
-              className="input w-96 text-2xl bg-white"
-              type="submit"
-              value="Sign up"
-            />
-            {passwordError && (
-              <div className="text-red-500 text-xs">{passwordError}</div>
-            )}
-            <br />
-          </form>
+          {verifyToggle ? (
+            <form onSubmit={onSubmitVerifyCode}>
+              <div className="input mb-2 w-96 text-2xl bg-gray-300">
+                {signUpEmail}
+              </div>
+              <br />
+              <input
+                className="input mb-2 w-96 text-2xl"
+                type="text"
+                placeholder="Verify code"
+                value={verifyCode}
+                onChange={onChangeVerifyCode}
+                maxLength={10}
+              />
+              <br />
+              <input
+                className="input w-96 text-2xl bg-white"
+                type="submit"
+                value="Verify"
+              />
+            </form>
+          ) : (
+            <form onSubmit={onSubmitSignup}>
+              <input
+                className="input mb-2 w-96 text-2xl"
+                type="text"
+                placeholder="Email"
+                value={signUpEmail}
+                onChange={onChangeSignUpEmail}
+              />
+              <br />
+              <input
+                className="input mb-2 w-96 text-2xl"
+                type="text"
+                placeholder="Nickname"
+                value={nickname}
+                onChange={onChangeNickname}
+                maxLength={10}
+              />
+              <br />
+              <input
+                className="input mb-2 w-96 text-2xl"
+                type="password"
+                placeholder="Password"
+                value={signUpPassword}
+                onChange={onChangeSignUpPassword}
+              />
+              <br />
+              <input
+                className="input mb-2 w-96 text-2xl"
+                type="password"
+                placeholder="Password check"
+                value={passwordCheck}
+                onChange={onChangePasswordCheck}
+              />
+              <br />
+              <input
+                className="input w-96 text-2xl bg-white"
+                type="submit"
+                value="Sign up"
+              />
+              {passwordError && (
+                <div className="text-red-500 text-xs">{passwordError}</div>
+              )}
+            </form>
+          )}
         </div>
         <div className="font-bold text-4xl mb-2">Login</div>
         <form onSubmit={onSubmitLogin}>
